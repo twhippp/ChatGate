5d95f72d31179ac0d5bf2253716316a49b34a883
twhippp <thefreshlycutgrass@gmail.com>
2026-03-27 00:35:06 -0500

    CreateExe Package import update

--- CreateEXE.bat
+++ CreateEXE.bat
@@ -1,1 +1,1 @@
-python -m PyInstaller --onefile --windowed --name ChatGate --icon=ChatGate.ico --add-data "ChatGate.ico;." main.py
+python -m PyInstaller --onefile --windowed --name ChatGate --icon=ChatGate.ico --add-data "ChatGate.ico;." --hidden-import pytchat --hidden-import PyQt5.QtSvg main.py
